25dc9e60931a52125c42c6e1a862cb9ef1656073
sanjeev-txt <txt.sanjeev@gmail.com>
2024-04-29 13:29:31 +0530

    vbvhg

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,20 @@
-pip3 install -r requirements.txt
-pyhton manage.py collectstatic 
+#!/bin/bash
+
+# Create and activate a virtual environment
+echo "Setting up virtual environment..."
+python3 -m venv myenv
+source myenv/bin/activate
+
+# Install dependencies
+echo "Installing Python dependencies..."
+pip install -r requirements.txt
+
+# Run collectstatic
+echo "Running collectstatic..."
+python manage.py collectstatic
+
+# Deactivate the virtual environment
+echo "Deactivating virtual environment..."
+deactivate
+
+echo "Build process completed."
