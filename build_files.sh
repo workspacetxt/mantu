a8f988640c176f69556ae1963c59ab3fc1810d55
sanjeev-txt <txt.sanjeev@gmail.com>
2024-04-29 15:14:47 +0530

    qqqq

--- build_files.sh
+++ build_files.sh
@@ -1,29 +1,17 @@
 #!/bin/bash
 
-# Path to your Python interpreter (adjust as needed)
-PYTHON="python3.9"
-
-# Path to your Django project directory
-DJANGO_PROJECT_DIR="dsiplay"
-
-# Path to your requirements.txt file
-REQUIREMENTS_FILE="requirements.txt"
-
 # Create and activate a virtual environment
 echo "Setting up virtual environment..."
-$PYTHON -m venv myenv
+python3 -m venv myenv
 source myenv/bin/activate
 
 # Install Python dependencies
 echo "Installing Python dependencies..."
-pip install -r $REQUIREMENTS_FILE
-
-# Navigate to the Django project directory
-cd $DJANGO_PROJECT_DIR
+pip install -r requirements.txt
 
 # Run collectstatic without interactive prompt
 echo "Running collectstatic..."
-$PYTHON manage.py collectstatic --no-input
+python3.9 manage.py collectstatic --no-input
 
 # Deactivate the virtual environment
 echo "Deactivating virtual environment..."
