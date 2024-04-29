f8278034c1745ac179c3c16611249d38f72078f0
sanjeev-txt <txt.sanjeev@gmail.com>
2024-04-29 13:38:00 +0530

    kjk

--- build_files.sh
+++ build_files.sh
@@ -5,13 +5,13 @@ echo "Setting up virtual environment..."
 python3 -m venv myenv
 source myenv/bin/activate
 
-# Install dependencies
+# Install Python dependencies
 echo "Installing Python dependencies..."
 pip install -r requirements.txt
 
-# Run collectstatic
+# Run collectstatic without interactive prompt
 echo "Running collectstatic..."
-python manage.py collectstatic
+python manage.py collectstatic --no-input
 
 # Deactivate the virtual environment
 echo "Deactivating virtual environment..."
